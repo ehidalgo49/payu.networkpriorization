de9307baff941dfdae2b8da1a2ccd3b6fb759ecc
ehidalgo49 <ehidalgo49@hotmail.com>
2025-02-28 00:25:07 -0500

    Initial Commit

---
+++ src/main/java/com/payu/networkpriorization/NetworkpriorizationApplication.java
@@ -0,0 +1,13 @@
+package com.payu.networkpriorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkpriorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkpriorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payu/networkpriorization/NetworkpriorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payu.networkpriorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkpriorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

348e430d7f7bf236213a124f3fcab843fc48a1e5
ehidalgo49 <ehidalgo49@hotmail.com>
2025-02-28 01:58:23 -0500

    Create Service Wired Controller Implement Service

--- src/main/java/com/payu/networkpriorization/controller/NetworkPrioritizationController.java
+++ src/main/java/com/payu/networkpriorization/controller/NetworkPrioritizationController.java
@@ -2,6 +2,8 @@ package com.payu.networkpriorization.controller;
 
 import com.payu.networkpriorization.model.NetworkRequest;
 import com.payu.networkpriorization.model.NetworkResponse;
+import com.payu.networkpriorization.service.NetworkPrioritizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/network")
 public class NetworkPrioritizationController {
+
+    private final NetworkPrioritizationService networkService;
+    @Autowired
+    public NetworkPrioritizationController(NetworkPrioritizationService networkService) {
+        this.networkService = networkService;
+    }
+
     @PostMapping("/prioritize")
     public ResponseEntity<NetworkResponse> prioritizeNetwork(@RequestBody NetworkRequest request){
-        int[] prioritize = {};
+        int[] prioritize = networkService.prioritizeNetwork(request.getResponseTimes(), request.getCosts(), request.getCriteria());
         NetworkResponse response = new NetworkResponse(prioritize);
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/payu/networkpriorization/service/NetworkPrioritization.java
+++ src/main/java/com/payu/networkpriorization/service/NetworkPrioritization.java
@@ -1,7 +1,42 @@
 package com.payu.networkpriorization.service;
 
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
 public class NetworkPrioritization {
+
+    /**
+     * Sorts the payment network indexes by the specified criteria using streams.
+     * If criteria is "RESPONSE_TIME", sort by response time (ascending) and in case of a tie, use cost (ascending).
+     * If criteria is "COST", sort by cost (ascending) and in case of a tie, use response time (ascending).
+     *
+     * @param responseTimes array of response times
+     * @param costs array of costs in USD cents
+     * @param criteria sorting criteria ("RESPONSE_TIME" or "COST")
+     * @return array of payment network indexes sorted by the criteria
+     */
     public static int[] prioritizeNetwork(int[] responseTimes, int[] costs, String criteria) {
-        return null;
+        if (responseTimes == null || costs == null || criteria == null) {
+            throw new IllegalArgumentException("Input arrays and criteria cannot be null");
+        }
+        if (responseTimes.length != costs.length) {
+            throw new IllegalArgumentException("Response time and cost arrangements must be of the same length");
+        }
+
+        Comparator<Integer> comparator;
+        if("RESPONSE_TIME".equalsIgnoreCase(criteria)){
+            comparator = Comparator.comparingInt((Integer i) -> responseTimes[i])
+                    .thenComparingInt(i -> costs[i]);
+        } else if("COST".equalsIgnoreCase(criteria)){
+            comparator = Comparator.comparingInt((Integer i) -> costs[i])
+                    .thenComparingInt(i -> responseTimes[i]);
+        } else {
+            throw new IllegalArgumentException("Invalid criteria: " + criteria);
+        }
+        return IntStream.range(0, responseTimes.length)
+                .boxed()
+                .sorted(comparator)
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
